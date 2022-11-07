12c6fc78bb0c728765b86b004f0acb0c00e5ad50
2Nadim <nadimmahamud661@gmail.com>
2022-11-07 15:58:36 +0000

    footer added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Home = () => {
+const Header = () => {
     return (
         <div>
-            <h1>this is home</h1>
+            <h1>header</h1>
         </div>
     );
 };
 
-export default Home;
+export default Header;
